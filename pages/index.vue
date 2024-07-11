b38b1bd90b9f93f447082501b173e27b526ba48b
mknkgwr1005 <mknkgwr@yahoo.co.jp>
2024-07-11 09:22:29 +0900

    ナビゲーションバーを設置

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,30 @@
 <template>
-  index
-  <ProductList />
+  <div>
+    <client-only>
+      <v-tabs v-model="tab" align-tabs="center" bg-color="red-lighten-2">
+        <v-tab value="home">Home</v-tab>
+        <v-tab value="register">登録管理画面</v-tab>
+      </v-tabs>
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item value="home">
+          <template v-if="tab === 'home'">
+            <ProductList />
+          </template>
+        </v-tabs-window-item>
+        <v-tabs-window-item value="register">
+          <template v-if="tab === 'register'">
+            <RegisterProducts />
+          </template>
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </client-only>
+  </div>
 </template>
 
 <script setup lang="ts">
-import ProductList from "../pages/ProductList.vue";
+import { ProductList, RegisterProducts } from "#components";
+
+const tab = ref("home");
 </script>
 
-<style></style>
+<style scoped></style>
