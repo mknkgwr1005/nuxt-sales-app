a1327f04bd44d231b1938aa962531c77cd777197
mknkgwr1005 <mknkgwr@yahoo.co.jp>
2024-07-12 14:56:57 +0900

    関数を修正

--- app.vue
+++ app.vue
@@ -12,7 +12,7 @@ const store = useIndexStore();
 /**
  * DBから登録している商品を取得してくる
  */
-const getRegiserDatafromDB = store.fetchRegisterData;
+const getRegiserDatafromDB = () => store.fetchRegisterData();
 
 /**
  * 登録した商品を定期的に検索する
@@ -21,8 +21,7 @@ let intervalId: number | null = null;
 
 const searchRegisteredProducts = async () => {
   console.log("searchingRegisteredProducts");
-  // この部分で registerData の更新をしないか、必要であれば特定の更新のみ行う
-  getRegiserDatafromDB;
+  getRegiserDatafromDB(); // 括弧を追加して関数を実行
   const getProduct = async () => await store.getRegisteredProducts();
   if (intervalId === null) {
     intervalId = window.setInterval(getProduct, 6000);
@@ -30,26 +29,14 @@ const searchRegisteredProducts = async () => {
 };
 
 // コンポーネントがマウントされたときに商品を検索する
-onMounted(() => searchRegisteredProducts());
-
-// registerDataが更新されたら、再度商品を検索する
-// watch(
-//   () => store.registerData,
-//   (newVal: any, oldVal: any) => {
-//     if (JSON.stringify(newVal) !== JSON.stringify(oldVal)) {
-//       console.log("registerData has been changed");
-//       searchRegisteredProducts();
-//       refreshNuxtData();
-//     }
-//   },
-//   { deep: true } // 深いウォッチを有効にする
-// );
+onMounted(() => {
+  searchRegisteredProducts();
+});
 
 // コンポーネントがアンマウントされたときにインターバルをクリアする
-// onUnmounted(() => {
-//   if (intervalId !== null) {
-//     console.log("unmounted");
-//     clearInterval(intervalId);
-//   }
-// });
+onUnmounted(() => {
+  if (intervalId !== null) {
+    clearInterval(intervalId);
+  }
+});
 </script>
