a384e1618e4492c2ced04ef09e58d62f39cee6b0
mknkgwr1005 <mknkgwr@yahoo.co.jp>
2025-04-22 11:33:25 +0900

    Revert "envを修正"
    This reverts commit c85a4541b3118a1c6e668a1605abd7ec3bf3b289.

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,21 +1,9 @@
 import axios from "axios";
-
+//リクエストURLを指定する この場合はlocalhostのあとに、
+// /api/をつけてあとはconfigにあるURLを追加する
 export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig();
-
   const instance = axios.create({
-    baseURL: config.public.API_BASE_URL as string, // envファイルに応じて切り替わる
+    baseURL: "/api",
   });
-
   nuxtApp.provide("axios", instance);
 });
-
-// import axios from "axios";
-// //リクエストURLを指定する この場合はlocalhostのあとに、
-// // /api/をつけてあとはconfigにあるURLを追加する
-// export default defineNuxtPlugin((nuxtApp) => {
-//   const instance = axios.create({
-//     baseURL: "/api",
-//   });
-//   nuxtApp.provide("axios", instance);
-// });
